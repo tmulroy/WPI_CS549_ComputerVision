e22b291baee8686d5c8f1a30e5acddd24e2046c1
tmulroy <thomasjohnmulroy@gmail.com>
2023-09-20 13:13:39 -0400

    add working Parts 1 and 2

--- week_4/transforms.py
+++ week_4/transforms.py
@@ -5,11 +5,12 @@ import numpy as np
 img = cv.imread('UnityHall.png')
 assert img is not None, "file could not be read, check with os.path.exists()"
 
+# Determines if application should save images to local directory
+save_flag = True
+
 # Image Information
 rows, cols, ch = img.shape
 smaller_img = cv.resize(img, None, fx=0.5, fy=0.5, interpolation=cv.INTER_CUBIC)
-print(f'rows: {rows}')
-print(f'cols: {cols}')
 
 # Helper Functions
 def harris_detection(image):
@@ -20,22 +21,24 @@ def harris_detection(image):
     image[dilated>0.04*dilated.max()] = [0, 0, 255]
     return image
 
-# ORIGINAL
-# cv.imshow('Original', img)
+sift = cv.SIFT_create()
+
+def sift_detection(image, sift=sift):
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    kp = sift.detect(gray, None)
+    sifted_img = cv.drawKeypoints(gray, kp, image)
+    return sifted_img
 
 # SCALE UP BY 20%
 scaled_up_img = cv.resize(img, None, fx=1.2, fy=1.2, interpolation = cv.INTER_CUBIC)
-# cv.imshow('Scaled Up', scaled_up_img)
 
 # SCALE DOWN BY 20%
 scaled_down_img = cv.resize(img, None, fx=0.8, fy=0.8, interpolation = cv.INTER_CUBIC)
-# cv.imshow('Scaled Down', scaled_down_img)
 
 # ROTATE by 10 degrees
 # cols-1 and rows-1 are the coordinate limits.
 M = cv.getRotationMatrix2D(((cols-1)/2.0,(rows-1)/2.0),10,1)
 rotated_img = cv.warpAffine(img,M,(cols,rows))
-# cv.imshow('Rotated',rotated_img)
 
 # AFFINE TRANSFORM
 # Scale down to fit in window
@@ -43,7 +46,6 @@ pts1 = np.float32([[0,0],[0,590],[984,0]])
 pts2 = np.float32([[0,50],[0,640],[984,-50]])
 M = cv.getAffineTransform(pts1,pts2)
 affine_img = cv.warpAffine(smaller_img,M,(cols,rows))
-# cv.imshow('Affine', affine_img)
 
 # PERSPECTIVE TRANSFORM
 # Using scaled-down image from affine transform
@@ -51,39 +53,29 @@ pers_pts1 = np.float32([[0,0], [984,10],[10,590],[590,980]])
 pers_pts2 = np.float32([[50,50], [3000,0],[10,590],[700,1500]])
 M = cv.getPerspectiveTransform(pers_pts1, pers_pts2)
 perspective_img = cv.warpPerspective(smaller_img,M,(cols,rows))
-# cv.imshow('Perspective', perspective_img)
 
-# EDGE DETECTION
-# Same sift for each image:
-sift = cv.SIFT_create()
+# Show images
 
-# Original Image Harris Corner Detection
-harris_original = harris_detection(img)
-cv.imshow('Harris Original', harris_original)
+images = {
+    'Original': img,
+    'Rotated': rotated_img,
+    'Scaled Up': scaled_up_img,
+    'Scaled Down': scaled_down_img,
+    'Affine': affine_img,
+    'Perspective': perspective_img
+}
 
-# Original Image SIFT Feature Detection
-# kp_original = sift.detect(gray_original,None)
-# sift_original = cv.drawKeypoints(gray_original,kp_original,img)
-# cv.imshow('Original SIFT', sift_original)
-
-
-# Rotated Image Harris Corner Detection
-rotated_harris = harris_detection(rotated_img)
-cv.imshow('Corners for Rotated Image', rotated_harris)
-
-# Rotated SIFT Feature Detection
-kp = sift.detect(rotated_img, None)
-gray_sift_rotated = cv.cvtColor(rotated_img,cv.COLOR_BGR2GRAY)
-rotated_sift_img = cv.drawKeypoints(gray_sift_rotated, kp, rotated_img)
-cv.imshow('SIFT Rotated', rotated_sift_img)
-
-# Scaled Up Harris Corner Detection
-scaled_up_harris = harris_detection(scaled_up_img)
-cv.imshow('Scaled Up Harris Corner Detection', scaled_up_harris)
-
-# Scaled Up SIFT Feature Detection
+if save_flag:
+    for name, img in images.items():
+        cv.imwrite(f'{name}.jpg', img)
+        harris = harris_detection(img)
+        cv.imwrite(f'{name}_Harris.jpg', harris)
+        sift = sift_detection(img)
+        cv.imwrite(f'{name}_SIFT.jpg', sift)
 
 k = cv.waitKey()
 if k == 27:  # Exit Program
     print('Exiting program')
     cv.destroyAllWindows()
+else:
+    pass
