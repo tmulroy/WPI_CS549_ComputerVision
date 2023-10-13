164a9f3cadbf33039ee8103fda601ed983178585
tmulroy <thomasjohnmulroy@gmail.com>
2023-10-13 18:43:49 -0400

    add hdr function

--- week_7/VisionCoin.py
+++ week_7/VisionCoin.py
@@ -9,7 +9,6 @@ def detect_coins(image):
 
     # Perform Hough Circle Transform
     rows = gray.shape[0]
-    print(rows)
     circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, 1, rows/4,
                               param1=100, param2=50,
                               minRadius=100, maxRadius=200)
@@ -24,66 +23,71 @@ def detect_coins(image):
         # circle outline
         radius = i[2]
         diameter = radius*2
-        print(diameter)
         # print(radius)
         cv.circle(image, center, radius, (255, 0, 255), 3)
 
     return image, circles
 
-def recognize_coins(src, info):
-    # Uses known images of coins to determine if they are in the input image
-    penny_img = cv.imread('penny.png')
-    nickel_img = cv.imread('nickel.png')
-    dime_img = cv.imread('dime.png')
-    quarter_img = cv.imread('quarter.png')
 
-    # Convert Known Coin Images to Gray and Blur to Reduce Noise
-    gray_penny = cv.cvtColor(penny_img, cv.COLOR_BGR2GRAY)
-    gray_penny = cv.medianBlur(gray_penny, 5)
-
-    gray_nickel = cv.cvtColor(nickel_img, cv.COLOR_BGR2GRAY)
-    gray_nickel = cv.medianBlur(gray_nickel, 5)
-
-    gray_dime = cv.cvtColor(dime_img, cv.COLOR_BGR2GRAY)
-    gray_dime = cv.medianBlur(gray_dime, 5)
-
-    gray_quarter = cv.cvtColor(quarter_img, cv.COLOR_BGR2GRAY)
-    gray_quarter = cv.medianBlur(gray_quarter, 5)
-
-    gray_input = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
-    gray_input = cv.medianBlur(gray_input, 5)
-
-    # Instantiate Feature Detection and Matching Algorithm Classes
-    sift = cv.SIFT_create()
-    bf = cv.BFMatcher()
-    surf = cv.xfeatures2d.SURF_create(1000)
-
-    # SIFT Feature Detection and Description
-    kp_penny, des_penny = surf.detectAndCompute(gray_penny, None)
-    kp_nickel, des_nickel = surf.detectAndCompute(gray_nickel, None)
-    kp_dime, des_dime = surf.detectAndCompute(gray_dime, None)
-    kp_quarter, des_quarter = surf.detectAndCompute(gray_quarter, None)
-    kp_input, des_input = surf.detectAndCompute(gray_input, None)
-
-    # Brute Force Matching
-    # Penny
-    penny_matches = bf.match(des_penny, des_input)
-    penny_matches = sorted(penny_matches, key=lambda x:x.distance)
-    penny_matches_img = cv.drawMatches(gray_penny, kp_penny, src, kp_input, penny_matches[:10], None,
-                             flags=cv.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
-    cv.imshow('penny matches', penny_matches_img)
-    cv.waitKey()
-
-    # Nickel
-    nickel_matches = bf.match(des_nickel, des_input)
-    nickel_matches = sorted(nickel_matches, key=lambda x: x.distance)
-    nickel_matches_img = cv.drawMatches(gray_nickel, kp_nickel, src, kp_input, nickel_matches[:10], None,
-                                       flags=cv.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
-    cv.imshow('nickels', nickel_matches_img)
-    cv.waitKey()
-
+def recognize_coins_ratios(src, circles):
     # Note: Using SIFT/SURF and Brute Force/Flann doesn't result in good matches.
     # I use the ratio of the coin diameters
+    # print(f'circles info: {circles}')
+    coins = {
+        'penny': {
+            'diameter': 19.05,
+            'value': 0.01,
+            'ratio': 1.0636,
+            'count': 0
+        },
+        'nickel': {
+            'diameter': 21.21,
+            'value': 0.05,
+            'ratio': 1.1842,
+            'count': 0
+        },
+        'dime': {
+            'diameter': 19.05,
+            'value': 0.1,
+            'ratio': 1,
+            'count': 0
+        },
+        'quarter': {
+            'diameter': 26.5,
+            'value': 0.25,
+            'ratio': 1.4796,
+            'count': 0
+        }
+    }
+
+    ratios = np.asarray([1.0636, 1.1842, 1, 1.4796])
+    ratios_names = ['penny', 'nickel', 'dime', 'quarter']
+    diameters = []
+    sum = 0
+    for circle in circles[0]:
+        diameter = circle[2]*2
+        diameters.append(diameter)
+
+    min_diameter = min(diameters)
+
+    for coin in circles[0]:
+        print('\n')
+        diameter = coin[2]*2
+        ratio = round(diameter/min_diameter, 4)
+        idx = (np.abs(ratios - ratio)).argmin()
+        coins[ratios_names[idx]]['count'] += 1
+        sum += coins[ratios_names[idx]]['value']
+    return round(sum, 2)
+
+
+# Part 2
+def hdr():
+    img_fn = ["IMAGE_1.JPG", "IMAGE_2.JPG", "IMAGE_3.JPG"]
+    img_list = [cv.imread(fn) for fn in img_fn]
+    merge_mertens = cv.createMergeMertens()
+    res_mertens = merge_mertens.process(img_list)
+    res_mertens_8bit = np.clip(res_mertens * 255, 0, 255).astype('uint8')
+    cv.imwrite("fusion_mertens.jpg", res_mertens_8bit)
 
 
 
@@ -91,7 +95,7 @@ if __name__ == '__main__':
     filename = 'coins_test.png'
     src = cv.imread(cv.samples.findFile(filename), cv.IMREAD_COLOR)
     circle_img, circles_info = detect_coins(src)
-    recognize_coins(src, circles_info)
-    # cv.imshow('circles', circle_img)
-    # cv.waitKey()
+    sum = recognize_coins_ratios(src, circles_info)
+    print(f'${sum}')
+    hdr()
 
